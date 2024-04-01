a2cc263659449d51f677e55201ecc0bb54954d43
akdemirsafak <akdemirsafak@gmail.com>
2024-04-01 17:10:51 +0300

    Proje created with class libs.

---
+++ Services/Location/Location.API/Controllers/CityController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Location.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok();
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            return Ok();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create()
+        {
+            return Ok();
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(Guid id)
+        {
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            return Ok();
+        }
+    }
+}
