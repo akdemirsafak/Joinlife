508d0136954ad4fa07dae7a3517d41a81f0d9637
akdemirsafak <akdemirsafak@gmail.com>
2024-04-05 16:19:44 +0300

    OrderService basit anlamıyla oluşturuldu.

---
+++ Services/Order/Order.Repository/Repositories/OrderRepository.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using Order.Domain.Repositories;
+using Order.Repository.DbContexts;
+
+namespace Order.Repository.Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly OrderDbContext _context;
+
+        public OrderRepository(OrderDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Domain.Entity.Order> CreateAsync(Domain.Entity.Order order)
+        {
+            await _context.Orders.AddAsync(order);
+            await _context.SaveChangesAsync();
+            return order;
+        }
+
+        public async Task<List<Domain.Entity.Order>> GetAllAsync()
+        {
+            return await _context.Orders.Include(x => x.OrderItems).ToListAsync();
+        }
+
+        public async Task<Domain.Entity.Order> GetByIdAsync(Guid id)
+        {
+            return await _context.Orders.Include(x => x.OrderItems).SingleOrDefaultAsync(x => x.Id == id);
+        }
+    }
+}
