9e79ff4379b0fbc4f7d21740a680454a5c33c187
akdemirsafak <akdemirsafak@gmail.com>
2024-04-11 01:07:06 +0300

    logout eklendi.Kullanıcı giriş yaptığında navbar'da değişiklik yapıldı.

--- Frontend/Joinlife.webui/Controllers/AuthController.cs
+++ Frontend/Joinlife.webui/Controllers/AuthController.cs
@@ -1,5 +1,7 @@
 ﻿using Joinlife.webui.Core.Services;
 using Joinlife.webui.Models.Auth;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Joinlife.webui.Controllers;
@@ -24,7 +26,7 @@ public class AuthController : Controller
        var result= await _identityService.SignInAsync(model);
         if (result.StatusCode==200)
         {
-            return RedirectToAction("Index", nameof(HomeController));
+            return RedirectToAction("Index","Home");
         }
         return View(model);
     }
@@ -37,7 +39,13 @@ public class AuthController : Controller
     {
         var result=await _identityService.SignUpAsync(model);
         if (result.StatusCode == 201)
-            return RedirectToAction("Index", nameof(HomeController));
+            return RedirectToAction("Index", "Home");
         return View(model);
+    }
+    public async Task<IActionResult> LogOut()
+    {
+        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme); //Cookie'leri siliyoruz.
+        await _identityService.RevokeRefreshToken();
+        return RedirectToAction(nameof(HomeController.Index), "Home");
     }
 }
